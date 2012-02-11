746ca6d4958692da08dd16c91e3ac7eeefc77b35
vhochstein <v.hochstein@highstone.de>
2012-02-11 18:48:50 +0100

    use engine initializer and lazy loading

--- lib/record_select/engine.rb
+++ lib/record_select/engine.rb
@@ -1,4 +1,26 @@
 module RecordSelect
   class Engine < Rails::Engine
+    initializer('initialize_record_select' ,:after => "initialize_active_scaffold") do
+      ActiveSupport.on_load(:action_controller) do
+        require 'record_select/extensions/localization'
+        require 'record_select/extensions/routing_mapper'
+        require 'record_select/actions'
+        require 'record_select/conditions'
+        require 'record_select/config'
+
+        include RecordSelect
+      end
+
+      ActiveSupport.on_load(:action_view) do
+        require 'record_select/form_builder'
+        require 'record_select/helpers/record_select_helper'
+        include RecordSelectHelper
+        ActionView::Helpers::FormBuilder.send(:include, RecordSelect::FormBuilder)
+      end
+
+      ActiveSupport.on_load(:active_record) do
+        require 'record_select/extensions/active_record'
+      end
+    end
   end
 end
--- lib/recordselect.rb
+++ lib/recordselect.rb
@@ -1,14 +1,3 @@
 require 'record_select'
-require 'record_select/extensions/localization'
-require 'record_select/extensions/active_record'
-require 'record_select/extensions/routing_mapper'
-require 'record_select/actions'
-require 'record_select/conditions'
-require 'record_select/config'
-require 'record_select/form_builder'
-require 'record_select/helpers/record_select_helper'
-require 'record_select/engine' unless defined? RECORD_SELECT_PLUGIN
 
-ActionController::Base.send(:include, RecordSelect)
-ActionView::Base.send(:include, RecordSelectHelper)
-ActionView::Helpers::FormBuilder.send(:include, RecordSelect::FormBuilder)
+require 'record_select/engine'
